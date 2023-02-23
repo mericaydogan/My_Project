ba1f8ccd89f5425214dbb0715ebad97abfbc9fca
Meriç <111490745+mericaydogan@users.noreply.github.com>
2023-02-23 18:08:50 +0300

    Add files via upload

---
+++ CA_PersonListt/CA_PersonListt/Personel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_PersonListt
+{
+    internal class Personel:Person
+    {
+        public string Department { get; set; }
+    }
+}
---
+++ CA_PersonListt/CA_PersonListt/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CA_PersonListt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Personlist adinda bir class tanimlayarak ardindan 3 adet person olusturup bu personlari bir liste icerisinde saklayin.
+            // console.writeline icerisinde person in bulundugu butun bilgilerini gosterin.
+
+            Personel person1 = new Personel();
+            person1.FirstName = "Emir";
+            person1.LastName = "Guven";
+            person1.Department = "Satis";
+
+            Personel person2 = new Personel();
+            person2.FirstName = "Eren";
+            person2.LastName = "Guvercin";
+            person2.Department = "Gelistirme";
+
+            Personel person3 = new Personel();
+            person3.FirstName = "Murat";
+            person3.LastName = "kara";
+            person3.Department = "Muhasebe";
+
+            
+
+
+               
+
+
+        }  
+    }
+}
